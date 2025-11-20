1d45a483ec2b33b5f040d635bf63645abb9be43b
antongoncharik <ant.goncharik@gmail.com>
2025-11-20 14:02:44 +0300

    Change sem count

--- internal/service/kline/kline.go
+++ internal/service/kline/kline.go
@@ -193,7 +193,7 @@ func (k *Kline) ProcessHistory(ctx context.Context, params entity.GetKlinesQuery
 		results[kline.Symbol] = append(results[kline.Symbol], kline)
 	}
 
-	sem := make(chan struct{}, 10)
+	sem := make(chan struct{}, 5)
 	var wgGRPC sync.WaitGroup
 	for resSymbol, resKlines := range results {
 		if len(resKlines) == 0 {
